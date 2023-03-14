5dc1fafc2fa22e0615dc3a8405c4de56c2c34b91
HæLL <israelaraujo.emply@gmail.com>
2023-03-14 13:02:37 -0300

    Concatenação de duas ou mais listas, utilizando a função ZIP.

---
+++ aula6.py
@@ -0,0 +1,11 @@
+# Função zip
+import tabnanny
+
+lista1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+lista2 = ['apartamento', 'carro', 'creche', 'condominio', 'pensao', 'seguro']
+lista3 = [630, 280.90, 400, 175.25, 300, 65]
+
+for numero, nome, valor in zip(lista1, lista2, lista3):
+#    print('Este é o número: {} ---> Item: {} e o valor: {:.2f}'. format(numero, nome, valor))
+
+lista1 []
